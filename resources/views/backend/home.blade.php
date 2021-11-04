441d5bb99307b6ad957ec77f05e93c800383d8ab
RezaulAlamOni <rzalam93@gmail.com>
2021-11-04 10:56:20 +0600

    Update code for new design

--- resources/views/backend/home.blade.php
+++ resources/views/backend/home.blade.php
@@ -78,6 +78,12 @@
 							id="size_setting_btn" style="margin-bottom: 0">
 							手書
 						</a></p>
+                    <br>
+						<p class="submessg">（受注）<a href="{{Config::get('app.url').'/onlineorder'}}"
+							class="btn btn-success btn-sm menualablebtn common_goto_sheet" data_num_link="12"
+							id="size_setting_btn" style="margin-bottom: 0; background: #0c673b !important;">
+                                ｵﾝﾗｲﾝ
+						</a></p>
 						<!-- <h4 class="btom_content_home" style="padding-top:55px">③000000000</h4> -->
 				</div>
 
