5826321172b1c19903a169378cc8f4d65fa11f08
a_sakil <jacossakil@gmail.com>
2021-12-08 21:38:04 +0600

    shop user added

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -16,41 +16,73 @@ class UsersTableSeeder extends Seeder
         $user_array=array(
             [
                 'name' => 'Jacos Super Admin',
+                'user_type'=>'super_admin',
                 'email' => 'super_admin@jacos.co.jp',
                 'password' => bcrypt('Qe75ymSr')
             ],
             [
                 'name' => 'Jacos Admin',
+                'user_type'=>'admin',
                 'email' => 'admin@jacos.co.jp',
                 'password' => bcrypt('Qe75ymSr')
             ],
             [
                 'name' => 'Tonya User',
+                'user_type'=>'tonya',
                 'email' => 'user@jacos.co.jp',
                 'password' => bcrypt('Qe75ymSr')
             ],[
                 'name' => 'Super User',
+                'user_type'=>'super',
                 'email' => 'jacos1@jacos.co.jp',
                 'password' => bcrypt('Qe75ymSr')
             ],[
                 'name' => 'Super User',
+                'user_type'=>'super',
                 'email' => 'jacos2@jacos.co.jp',
                 'password' => bcrypt('Qe75ymSr')
             ]
             ,[
                 'name' => 'Super User',
+                'user_type'=>'super',
                 'email' => 'jacos3@jacos.co.jp',
                 'password' => bcrypt('Qe75ymSr')
             ],
             [
                 'name' => 'Tonya User',
+                'user_type'=>'tonya',
                 'email' => 'tonya2@jacos.co.jp',
                 'password' => bcrypt('Qe75ymSr')
             ],[
                 'name' => 'Tonya User',
+                'user_type'=>'tonya',
                 'email' => 'tonya3@jacos.co.jp',
                 'password' => bcrypt('Qe75ymSr')
             ],
+            [
+                'name' => 'Super User',
+                'email' => 'shop_a@jacos.co.jp',
+                'user_type'=>'shop',
+                'password' => bcrypt('123456')
+            ],
+            [
+                'name' => 'Super User',
+                'user_type'=>'shop',
+                'email' => 'shop_b@jacos.co.jp',
+                'password' => bcrypt('654321')
+            ],
+            [
+                'name' => 'Super User',
+                'user_type'=>'shop',
+                'email' => 'shop_c@jacos.co.jp',
+                'password' => bcrypt('333222')
+            ],
+            [
+                'name' => 'Super User',
+                'user_type'=>'shop',
+                'email' => 'shop_d@jacos.co.jp',
+                'password' => bcrypt('222333')
+            ]
         );
 
         $user_details_array=array(
@@ -62,6 +94,10 @@ class UsersTableSeeder extends Seeder
             ['users_id'=>6],
             ['users_id'=>7],
             ['users_id'=>8],
+            ['users_id'=>9],
+            ['users_id'=>10],
+            ['users_id'=>11],
+            ['users_id'=>12],
         );
         User::insert($user_array);
         users_details::insert($user_details_array);
