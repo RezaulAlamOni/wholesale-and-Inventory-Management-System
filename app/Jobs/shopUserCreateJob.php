8df3e48d5c18194905cbc738efdc510b447e571b
a_sakil <jacossakil@gmail.com>
2021-12-23 00:18:01 +0600

    jobcreatedupdated

--- app/Jobs/shopUserCreateJob.php
+++ app/Jobs/shopUserCreateJob.php
@@ -3,19 +3,23 @@
 namespace App\Jobs;
 
 use Illuminate\Foundation\Bus\Dispatchable;
-
+use App\User;
+use App\customer_shop;
+use Log;
 class shopUserCreateJob
 {
     use Dispatchable;
-
+    protected $user_info;
+    protected $shop_info;
     /**
      * Create a new job instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($user_info,$shop_info)
     {
-        //
+        $this->user_info = $user_info;
+        $this->shop_info = $shop_info;
     }
 
     /**
@@ -25,6 +29,20 @@ class shopUserCreateJob
      */
     public function handle()
     {
-        //
+        try{
+        Log::info(print_r("connect to super DB",true));
+        $User = new User;
+        $customer_shop = new customer_shop;
+
+        $User->setConnection('superv1');
+        $customer_shop->setConnection('superv1');
+        $user_id = $User->insertGetId($this->user_info);
+        $this->shop_info['user_id']=$user_id;
+        Log::info(print_r($this->shop_info,true));
+        Log::info(print_r($this->user_info,true));
+        $customer_shop->insert($this->shop_info);
+    }catch(\Exception $e){
+        Log::info($e->getMessage());
+    }
     }
 }
